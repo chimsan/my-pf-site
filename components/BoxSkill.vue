69100debafce69b094fe25ac1fd5375cc8dbdbc4
chimsan <chiminchi830@gmail.com>
2022-03-17 04:50:31 +0900

    リンク追加

--- components/BoxSkill.vue
+++ components/BoxSkill.vue
@@ -3,6 +3,10 @@
     h3(id="skill") Skill
     p
       | HTML5+CSS3 / Vue.js / Nuxt.js / JavaScript / TypeScript / Photoshop / Adobe XD / git / github
+    ul
+      li
+        a(href="https://github.com/chimsan/my-pf-site" target="_blank")
+          | 本サイトのリポジトリ - github
 
 </template>
 
@@ -11,3 +15,20 @@ export default {
   name: 'SkillBox'
 }
 </script>
+
+<style lang="scss" scoped>
+ul {
+  li {
+    list-style: circle;
+    line-height: 2.6rem;
+    list-style-position: inside;
+    a {
+      text-decoration: underline;
+    }
+  }
+}
+
+p {
+  margin-bottom: 2rem;
+}
+</style>
